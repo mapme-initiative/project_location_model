7e7682650a62caa5e5be6e472184950c81019847
ffischer1984 <8037710+ffischer1984@users.noreply.github.com>
2026-04-16 10:38:49 +0200

    Fix email body validation link
    Updated the validation link in the email body to point to the correct URL for the project location model.

--- frontend/src/components/SendMailButton.tsx
+++ frontend/src/components/SendMailButton.tsx
@@ -10,7 +10,7 @@ type SendMailButtonProps = {
 export default function SendMailButton(props: Readonly<SendMailButtonProps>) {
     const generateMailTo = () => {
         const subject = encodeURIComponent("Validated Location data for Project " + props.inProNumbers.join(", "));
-        const body = encodeURIComponent(`Dear sir or madam, \n\rThis email contains validated location data for the project(s): \n${props.inProNumbers.join(", \n")}\nIt was validated by the "Location Validator" on https://mapme-initiative.github.io/ogm-validator/. You can use the Location Validator Tool (https://mapme-initiative.github.io/ogm-validator/) yourself to check the data validity and see all project locations printed on a map.\nYours sincerly, `);
+        const body = encodeURIComponent(`Dear sir or madam, \n\rThis email contains validated location data for the project(s): \n${props.inProNumbers.join(", \n")}\nIt was validated by the "Location Validator" on https://mapme-initiative.github.io/project_location_model/validation.html. You can use the Location Validator Tool (https://mapme-initiative.github.io/project_location_model/validation.html) yourself to check the data validity and see all project locations printed on a map.\nYours sincerly, `);
         const mailtoLink = `mailto:?subject=${subject}&body=${body}`;
         window.location.href = mailtoLink;
     }
