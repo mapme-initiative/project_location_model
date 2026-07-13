60e54991a28c4463f36a207997f8e4260e25fb5c
UrAnt G. F. <gapfrey@yahoo.com>
2026-07-13 16:47:27 +0200

    fix(validator): remove excess type property from donor project warnings call

--- frontend/src/components/FileValidator.tsx
+++ frontend/src/components/FileValidator.tsx
@@ -162,7 +162,6 @@ export default function FileValidator(): React.ReactElement {
 							features: transformedFeatures,
 						});
 						setValidationWarnings(getDonorProjectWarnings({
-							type: "FeatureCollection",
 							features: stampedFeatures,
 						}));
 						break;
